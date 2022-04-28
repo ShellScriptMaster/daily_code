728da0275a8112f72dd5a52789fba7346ad570d8
guanziq <772768836@qq.com>
2022-04-28 04:38:45 +0000

    删除文件 day_01

--- day_01/day_01_Memory_address.py
+++
@@ -1,8 +0,0 @@
-#  Memory Position
-# To describe how variety take up memory
-name = 'Jack'  # define a variable and assign it.
-print(id(name))  # use a function id() to show which memory address was take up to save the variety assignment
-name1 = name  # create a variety and assign it by other variety
-print(id(name1), id(name))
-# different variety all point to the same MEM_address. In other words, it means all variety both  point to the value
-
