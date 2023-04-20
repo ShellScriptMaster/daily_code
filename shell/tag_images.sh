475dc94be2f5d9664760c3be0f02fd92b3fb53ac
shell_master <guanziq957@163.com>
2023-04-21 00:20:57 +0800

    enhanced

--- shell/tag_images.sh
+++ shell/tag_images.sh
@@ -30,16 +30,16 @@ docker images | awk 'NR>1{print$1}'  >> ORIGIN_IMG.txt
 docker images | awk 'NR>1{print$2}'  >> ORIGIN_TAG.txt
 awk -F/ '{print $2}'   ORIGIN_IMG.txt >> IMG_NAME.txt
 LINES=`sed -n '$=' ORIGIN_IMG.txt`
-for num in `seq 1 $LINES ` 
+for num in `seq 1 ${LINES} `
 do
 ORIGIN_IMG=`sed  -n  "${num}p"  ORIGIN_IMG.txt`
 ORIGIN_TAG=`sed  -n  "${num}p"  ORIGIN_TAG.txt`
 IMG_NAME=`sed -n "${num}p" IMG_NAME.txt`
 #docker tag   $ORIGIN_IMG:$ORIGIN_TAG  $REPO_PORT/$IMG_NAME:$ORIGIN_TAG
-echo   $ORIGIN_IMG:$ORIGIN_TAG  $REPO_PORT/$IMG_NAME:$ORIGIN_TAG
+echo   ${ORIGIN_IMG}:${ORIGIN_TAG} ${REPO_PORT}/${IMG_NAME}:${ORIGIN_TAG}
 
 #docker push  $REPO_PORT/$IMG_NAME:$ORIGIN_TAG
-echo $REPO_PORT/$IMG_NAME:$ORIGIN_TAG
+echo ${REPO_PORT}/${IMG_NAME}:${ORIGIN_TAG}
 
 #docker rmi ${ORIGIN_IMG}:${ORIGIN_TAG}
 echo ${ORIGIN_IMG}:${ORIGIN_TAG}
@@ -49,8 +49,5 @@ echo ${ORIGIN_IMG}:${ORIGIN_TAG}
 
 done
 
-
-
-
 rm -rf ORIGIN_IMG.txt ORIGIN_TAG.txt IMG_NAME.txt
-curl $REPO_PORT/v2/_catalog
+curl ${REPO_PORT}/v2/_catalog
